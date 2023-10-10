a1e7bd6689fc66893e9b3253d6b271ce26f79b62
rhit-huttonep <huttonep@rose-hulman.edu>
2023-10-09 23:08:22 -0400

    edited

--- portfolio/scripts/main.js
+++ portfolio/scripts/main.js
@@ -1,4 +1,22 @@
 console.log("Hello World");
-document.querySelector("#myButton").onclick = (event) => {
-    alert("This is important");
-};
+const scrollBtn = document.createElement("button");
+scrollBtn.innerHTML = "&uarr;";
+scrollBtn.setAttribute("id", "scroll-btn");
+document.body.appendChild(scrollBtn);
+
+// const scrollBtnDisplay = function () {
+//     window.scrollY > window.innerHeight
+//       ? scrollBtn.classList.add("show")
+//       : scrollBtn.classList.remove("show");
+//   };
+//   window.addEventListener("scroll", scrollBtnDisplay);
+
+  const scrollWindow = function () {  
+    if (window.scrollY != 0) {
+      setTimeout(function () {
+        window.scrollTo(0, window.scrollY - 50);
+        scrollWindow();
+      }, 10);
+    }
+  };
+  scrollBtn.addEventListener("click", scrollWindow);
